7f21325b913ef05aa24340265e751e12fb92ce6d
Johanne Marlowe E. Sanchez <119918311+APCjesanchez@users.noreply.github.com>
2023-02-17 14:21:48 +0800

    Re-Transfer

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,9 +1,7 @@
 <!doctype html>
 <html>  
 <head>
-    <?php echo link_tag('css/mystyle.css'); ?>
+    <?php echo link_tag('style.css'); ?>
     <title>CodeIgniter Tutorial</title>
 </head>
 <body>
-
-    <h1><?= esc($title) ?></h1>
