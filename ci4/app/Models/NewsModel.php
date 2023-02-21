c651289109060064a75c7ff13378cb7afc7069cc
Johanne Marlowe E. Sanchez <119918311+APCjesanchez@users.noreply.github.com>
2023-02-21 15:36:37 +0800

    News Label Section apcwebprogcsf

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 
 class NewsModel extends Model
 {
-    protected $table = 'news';
+    protected $table = 'jesanchez_news';
 
     public function getNews($slug = false)
     {
