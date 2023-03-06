415d97a8dd5c7d518cd3361e98f7598fc54bb76d
Johanne Marlowe E. Sanchez <119918311+APCjesanchez@users.noreply.github.com>
2023-03-06 09:31:54 +0800

    testing

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -23,6 +23,7 @@
             <li><a href = "/lab3/ci4/public/home#content-about">About</a></li>
             <li><a href = "/lab3/ci4/public/home#content-references">References</a></li>
             <li><a href = "/lab3/ci4/public/guest/create">Register</a></li>
+            <li><a href = "/lab3/ci4/public/guest">Members</a></li>
         <span id="time">
             <span id="hours">00</span>
             <span>:</span>
@@ -33,7 +34,7 @@
         </span>
         </ul>
     </nav>
-    <!--In this area of the javascript, I have already implemented loops, functions and HTML DOM in order to create an alarm clock that depended on the time zone of the device.-->
+<!--In this area of the javascript, I have already implemented loops, functions and HTML DOM in order to create an alarm clock that depended on the time zone of the device.-->
    <!--I have also implemented the camelCase for all functions or variables for better code readability-->
    <!--I have also implemented more "let" variables for the website to have better performance with ready to call variables-->
 
